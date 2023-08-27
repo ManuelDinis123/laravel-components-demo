04a81e737dc41d19a9fd5beb856fc0433eb49169
ManuelDinis <4228@feppv.pt>
2023-08-27 14:57:07 +0000

    - Added github link on navbar

--- resources/views/layouts/side.blade.php
+++ resources/views/layouts/side.blade.php
@@ -20,6 +20,7 @@
 <body id="body-pd">
     <header class="header" id="header">
         <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+        <div class="header_img" onclick="window.open('https\:\/\/github.com/ManuelDinis123/laravel-components-demo', '_blank').focus();"> <i class='bx bxl-github'></i> </div>
     </header>
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
